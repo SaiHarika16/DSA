27c4642662f32e12aaa3c4496bd9a82f19f9ba14
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-06-04 16:49:02 +0530

    Time: 4 ms (81.26%), Space: 13.5 MB (90.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,15 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        candidate=nums[0]
-        count=0
-        for num in nums:
-            if count==0:
-                candidate=num
-                count=1
-            elif num==candidate:
-                count+=1
-            else:
-                count-=1
-        return candidate
+        nums.sort()
+        return nums[len(nums)//2]
         
