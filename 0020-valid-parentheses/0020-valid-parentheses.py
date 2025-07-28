fe5cfce4bbc0e414af0ca4feab02ea6cb23f90b0
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-07-28 22:58:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        mappings={"]":"[",")":"(","}":"{"}
+        stack=[]
+        for i in s:
+            if i in mappings:
+                top_element=stack.pop()
+                if top_element!=mappings[i]:
+                    return False
+            else:
+                stack.append(i)
+        return len(stack)==0
+
+
+        
+        
