5f5e51c7595af9d9d60b99f6147f194b06893dd0
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-08-15 20:12:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        while n>1:
+            if n%4!=0:
+                return False
+            elif n%4==0:
+                n=n//4
+        if n==1:
+            return True
+        
