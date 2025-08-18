88ab55c7f25a206b478a1e8f978b0c416385d253
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-08-18 14:03:41 +0530

    Time: 4 ms (70.47%), Space: 19.2 MB (97.51%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,9 +5,24 @@ class Solution:
         for num,freq in c.items():
             if freq>n/2:
                 return num'''
-        d={}
+        '''d={}
         n=len(nums)
         for i in nums:
             d[i]=d.get(i,0)+1
             if d[i]>n/2:
-                return i      
+                return i'''
+        '''nums.sort()
+        n=len(nums)
+        return nums[n//2]'''
+        candidate=None
+        count=0
+        for num in nums:
+            if count==0:
+                candidate=num
+            if num==candidate:
+                count+=1
+            else:
+                count-=1
+        return candidate
+
+              
