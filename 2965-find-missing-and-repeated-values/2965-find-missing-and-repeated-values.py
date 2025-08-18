64aa789db828669e5a9b54734e04ab98c223df18
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-08-18 22:03:55 +0530

    Time: 0 ms (100%), Space: 18.1 MB (63.51%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
@@ -1,17 +1,25 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        freqs={}
-        numbers=len(grid)**2
+        n=len(grid)
+        expected_sum=((n**2)*(n**2+1))//2
+        expected_sum_of_squares=((n**2)*(n**2+1)*(2*n**2+1))//6
+        actual_sum=0
+        actual_sum_of_squares=0
         for i in grid:
             for j in i:
-                freqs[j]=freqs.get(j,0)+1
-        #repeating=0
-        #missing=0
-        for num in range(1,numbers+1):
-            if num not in freqs:
-                missing=num
-            elif freqs[num]==2:
-                repeating=num
-        return [repeating,missing]
+                actual_sum+=j
+                actual_sum_of_squares+=j*j
+        #b-a
+        #b is missing
+        #a is repeated
+        sum_diff=expected_sum-actual_sum
+        #b^2-a^2
+        sqr_diff=expected_sum_of_squares-actual_sum_of_squares
+        #b+a
+        sum_sum=(sqr_diff)//(sum_diff)
+        missing=(sum_diff+sum_sum)//2
+        repeated=missing-sum_diff
+        return [repeated,missing]
 
-        
+
+
