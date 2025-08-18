ab4a114aeef66f4685aa491827f5343ba46ad039
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-08-18 22:33:28 +0530

    Time: 11 ms (18.14%), Space: 19.8 MB (21.09%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res=0
+        '''res=0
         for num in nums:
             res^=num
-        return res
+        return res'''
+        d=defaultdict(int)
+        for num in nums:
+            d[num]+=1
+        for num,freq in d.items():
+            if freq==1:
+                return num
+
         
