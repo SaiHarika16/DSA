0b8496330b54824b6cc27c9b718dca186a5b3328
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-08-20 22:07:32 +0530

    Time: 8 ms (78.9%), Space: 17.7 MB (86.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
+        '''if len(s)!=len(t):
             return False
         sc=defaultdict(int)
         tc=defaultdict(int)
         for i in range(len(s)):
             sc[s[i]]=sc.get(s[i],0)+1
             tc[t[i]]=tc.get(t[i],0)+1
-        return sc==tc
-        re
+        return sc==tc'''
+        return Counter(s)==Counter(t)
