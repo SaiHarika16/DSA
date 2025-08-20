8bc5b40b58a7f0b191e56acca843644b5be2aad0
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-08-20 22:07:19 +0530

    Time: 16 ms (24.71%), Space: 17.7 MB (86.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,11 @@
-class Solution(object):
-    def isAnagram(self, s, t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        else:
-            a="abcdefghijklmnopqrstuvwxyz"
-            for i in a:
-                if(s.count(i)!=t.count(i)):
-                    return False
-                    break
-        return True
-        
+        sc=defaultdict(int)
+        tc=defaultdict(int)
+        for i in range(len(s)):
+            sc[s[i]]=sc.get(s[i],0)+1
+            tc[t[i]]=tc.get(t[i],0)+1
+        return sc==tc
+        re
