6b37b9bb33dc7b77641cc6887ae9868b2921d0f3
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-08-24 18:26:12 +0530

    Time: 11 ms (91.54%), Space: 12.5 MB (49.79%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n==0:
-            return 0
-        elif n==1:
-            return 1
-        else:
-            return self.fib(n-1)+self.fib(n-2)
+        a,b=0,1
+        for i in range(n):
+            a,b=b,a+b
+        return a
         
