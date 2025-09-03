5b80ec4c5f7837530acf0de940cc2bee9ae065c6
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-09-03 21:35:32 +0530

    Time: 0 ms (100%), Space: 17.6 MB (82.17%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.py
+++ 3024-type-of-triangle/3024-type-of-triangle.py
@@ -2,7 +2,7 @@ class Solution:
     def triangleType(self, nums: List[int]) -> str:
         nums.sort()
         for i in range(len(nums)):
-            if(nums[i]+nums[i+1]>nums[i+2] and nums[i]+nums[i+2]>nums[i+1] and nums[i+2]+nums[i+1]>nums[i]):
+            if(nums[i]+nums[i+1]>nums[i+2] and nums[i+1]+nums[i+2]>nums[i] and nums[i+2]+nums[i]>nums[i+1]):
                 if(nums[i]==nums[i+1]==nums[i+2]):
                     return "equilateral"
                 elif(nums[i]<nums[i+1]<nums[i+2]):
