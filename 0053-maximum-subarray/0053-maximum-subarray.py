1fd3ae8010af6bf03f3bddbf9519dad4a600689e
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-11-07 00:08:27 +0530

    Time: 95 ms (20.86%), Space: 32 MB (82.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,7 +3,6 @@ class Solution:
         curr_sum=nums[0]
         res_sum=nums[0]
         for i in range(1,len(nums)):
-            curr_sum=max(nums[i],nums[i]+curr_sum)
-            res_sum=max(curr_sum,res_sum)
-        return res_sum
-        
+            curr_sum=max(nums[i],curr_sum+nums[i])
+            res_sum=max(res_sum,curr_sum)
+        return res_sum  
