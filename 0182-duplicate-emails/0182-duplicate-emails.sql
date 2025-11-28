f4a5d497447f5395abcd9cfc6484afc380298433
Sai Harika Usikarla <132150772+SaiHarika16@users.noreply.github.com>
2025-11-28 14:02:05 +0530

    Time: 413 ms (14.05%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT email as Email FROM Person GROUP BY email HAVING count(email)>1
+SELECT email AS Email FROM Person GROUP BY email HAVING COUNT(email)>1;
